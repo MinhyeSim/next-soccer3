aa4c5930bfcc6782a4bd62c34ead9f5d5bf0ed68
MinhyeSim <smine0032@gmail.com>
2022-04-28 14:11:24 +0900

    14:11

--- component/Layout.js
+++ component/Layout.js
@@ -5,12 +5,9 @@ import { Header, Nav,Table,Pagination,Footer,Modal } from "@/component";
 
 export function Layout({ children }){
   return (<div className={styles.container}>
-        <Nav/>
         <main className={styles.main}>{children}</main>
-        <Header/>      
         <Table/>
         <Pagination/>
-        <Footer/>
         <Modal/>    
         </div>
   );
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import '@/styles/globals.css'
+import { wrapper } from '@/modules/store'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const App = ({ Component, pageProps }) => {
+  return <>
+  <Header/>
+  <Nav/>
+  <Component {...pageProps} />
+  <Footer/>
+  </>
+  
 }
 
-export default MyApp
+export default wrapper.withRedux(App)
